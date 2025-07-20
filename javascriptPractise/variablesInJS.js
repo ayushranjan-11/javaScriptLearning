f5acfe8ac1d25ee939070e9de1ac6c6c59906fd3
ayushranjan-11 <ayush.ranjan921@gmail.com>
2025-07-20 19:05:17 +0530

    Return back to learning

--- javascriptPractise/variablesInJS.js
+++ javascriptPractise/variablesInJS.js
@@ -3,4 +3,8 @@ var a = 12;
 
 function test(){
     var b = 14;
+    console.log(a);
+    console.log(b); //This is resticted under the function test() so accessing this variable is restricted inside of the function
 }
+test(); //This is required to call any function
+console.log(a); //As "a" is global variable it can be accessed anywhere
